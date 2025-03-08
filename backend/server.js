8d5b3f78ac059e0440b33b1a5e485bbe19aae31b
Thalita Teles <109113436+telessthalita@users.noreply.github.com>
2025-03-08 12:19:34 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,23 +6,13 @@ require('dotenv').config();
 
 const app = express();
 
-const allowedOrigins = process.env.FRONTEND_URL ? process.env.FRONTEND_URL.split(',') : ['http://localhost:3000'];
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: ['http://localhost:5174', 'http://localhost:3000', 'https://moodtunes-xtcy.onrender.com/'],
   methods: 'GET,POST,PUT,DELETE,OPTIONS',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true,
 };
-
 app.use(cors(corsOptions));
-
 app.use(express.json());
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY, { apiVersion: 'v1' });
